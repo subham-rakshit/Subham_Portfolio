693c664b0bdccd29676e0ff2818451cc446a4cc0
subham-rakshit <subhamrakshit667@gmail.com>
2024-08-25 00:20:23 +0530

    Add Dashboard Authentication Key UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
 import { FaGripLines } from "react-icons/fa";
 import { MdClose } from "react-icons/md";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Button, Modal } from "flowbite-react";
 
 function Header({ isFixed }) {
+  const [authKey, setAuthKey] = useState(null);
+  const [focusedInput, setFocusedInput] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
   const [isProfileClicked, setIsProfileClicked] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
   const [path, setPath] = useState(null);
   const location = useLocation();
+  const navigate = useNavigate();
 
   // Onscroll navbar background change logic
   window.addEventListener("scroll", function () {
     let headerElem = document.querySelector(".navbar");
     headerElem.classList.toggle("sticky-nav", window.scrollY > 0);
   });
+
+  //? Authentication for ADMIN --->
+  // Handle placeholder gets disappear when specific input field gets focused
+  const handleOnFocus = () => {
+    setFocusedInput(true);
+  };
+
+  // Handle placeholder gets visible when specific input filed gets empty value
+  const handleOnBlur = (e) => {
+    const { value } = e.target;
+    if (!value) {
+      setFocusedInput(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setAuthKey(value);
+  };
+
+  const handleAdminAuth = () => {
+    if (authKey === "7980-1997") {
+      setOpenModal(false);
+      setAuthKey(null);
+      setFocusedInput(false);
+      navigate("/admin");
+    }
+  };
+
+  //? Authentication for ADMIN --->
 
   const handleProfile = () => {
     setIsProfileClicked((prev) => !prev);
@@ -114,14 +150,14 @@ function Header({ isFixed }) {
                   <span>subhamrakshit667@gmail.com</span>
                 </span>
 
-                <Link to="/admin">
-                  <button
-                    type="button"
-                    className="text-xs font-poppins text-zinc-500 hover:text-zinc-700 transition-all duration-200 ease-linear font-[500]"
-                  >
-                    Dashboard
-                  </button>
-                </Link>
+                <button
+                  type="button"
+                  className="text-xs font-poppins text-zinc-500 hover:text-zinc-700 transition-all duration-200 ease-linear font-[500]"
+                  onClick={() => setOpenModal(true)}
+                >
+                  Dashboard
+                </button>
+
                 <button
                   type="button"
                   className="text-xs font-poppins font-[500] text-blue-800 hover:text-red-500 transition-all duration-200 ease-linear"
@@ -133,7 +169,7 @@ function Header({ isFixed }) {
           </div>
         </div>
 
-        {/* Nav Items Container */}
+        {/* Mobile Nav Items Container */}
         <motion.div
           initial={{ y: "-100%", opacity: 0 }}
           animate={
@@ -158,7 +194,6 @@ function Header({ isFixed }) {
             ].map((item, index) => (
               <Link to={item.link} key={`${item.name}${index}`}>
                 <li
-                  key={`${item.name}${index}`}
                   className={`w-fit text-3xl text-[#FBF9ED] font-Founders_Grotesk_X-Condensed capitalize cursor-pointer transition-all duration-300 ${
                     path && path === item.link
                       ? "text-[#B8D253] font-bold"
@@ -187,13 +222,25 @@ function Header({ isFixed }) {
           {/* Social Links Mobile view */}
           <ul className="list-none pl-0 flex justify-between items-center gap-2 mt-5">
             {[
-              { name: "linkedin_1", href: "/linkedin.png", link: "/" },
-              { name: "github_2", href: "/github-light.png", link: "/about" },
-              { name: "facebook_3", href: "/facebook.png", link: "/projects" },
+              {
+                name: "linkedin_1",
+                href: "/linkedin.png",
+                link: "https://www.linkedin.com/in/subhamjitu97/",
+              },
+              {
+                name: "github_2",
+                href: "/github-light.png",
+                link: "https://github.com/subham-rakshit",
+              },
+              {
+                name: "facebook_3",
+                href: "/facebook.png",
+                link: "https://www.facebook.com/subham.rakshit.142",
+              },
               {
                 name: "instagram_4",
                 href: "/instagram.png",
-                link: "/contact-me",
+                link: "https://www.instagram.com/subham_rakshit_1/",
               },
             ].map((logo) => (
               <Link to={logo.link} key={logo.name}>
@@ -207,6 +254,47 @@ function Header({ isFixed }) {
             ))}
           </ul>
         </motion.div>
+
+        {/* Popup Modal */}
+        <Modal
+          show={openModal}
+          size="md"
+          onClose={() => setOpenModal(false)}
+          popup
+        >
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center flex flex-col gap-5">
+              <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+              <div className="relative w-full flex-1 group">
+                {!authKey && !focusedInput && (
+                  <span className="absolute left-1/2 top-full -translate-x-[50%] -translate-y-[100%] text-sm w-full text-center font-poppins text-zinc-200 group-hover:text-zinc-400 font-light py-1 pointer-events-none">
+                    Please provide the KEY *
+                  </span>
+                )}
+                <input
+                  type="text"
+                  id="authKey"
+                  name="authKey"
+                  value={authKey}
+                  onFocus={handleOnFocus}
+                  onBlur={handleOnBlur}
+                  onChange={handleInputChange}
+                  autoComplete="off"
+                  className="border-b-[1px] group-hover:border-b-[2px] border-zinc-500 group-hover:border-zinc-800 text-center font-semibold text-zinc-200 text-xl px-2 py-1 bg-transparent focus:outline-none focus:border-t-0 focus:border-r-0 focus:border-l-0 focus:ring-0 w-full cursor-pointer"
+                />
+              </div>
+              <div className="flex justify-center gap-4">
+                <Button color="success" onClick={handleAdminAuth}>
+                  Authenticate
+                </Button>
+                <Button color="failure" onClick={() => setOpenModal(false)}>
+                  No, cancel
+                </Button>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
+import flowbite from "flowbite-react/tailwind";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
       fontFamily: {
